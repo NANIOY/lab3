d3543103b5b0da0d8cd4707a648b2807d4f3eb4a
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-13 13:39:02 +0200

    feat: create quoteTypes

--- script/quote.js
+++ script/quote.js
@@ -1,15 +1,30 @@
 const apiUrl = 'https://api.quotable.io/random';
 
-function getQuote() {
-    return fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
-            console.error(error);
-            return null;
-        });
+const quoteTypes = {
+    'clear': 'inspire',
+    'clouds': 'management',
+    'rain': 'life',
+    'thunderstorm': 'funny',
+    'snow': 'art',
+    'fog': 'love',
+    'default': 'inspire',
+};
+
+async function getQuote(weatherCondition) {
+    const category = quoteTypes[weatherCondition] || 'inspire';
+    const apiUrlWithCategory = `${apiUrl}?category=${category}`;
+
+    try {
+        const response = await fetch(apiUrlWithCategory);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 }
 
-export { getQuote };
+export { getQuote, quoteTypes };
