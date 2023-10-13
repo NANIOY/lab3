efe4f89d933df6fe4001e14b1595cab79a182705
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-13 13:38:52 +0200

    feat: random quote display based on weather

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 import Weather from './weather.js';
 import { updateBannerBackground } from './banner.js';
-import { getQuote } from './quote.js'; // Import the getQuote function
+import { getQuote, quoteTypes } from './quote.js';
 
 class App {
     constructor() {
@@ -24,28 +24,31 @@ class App {
 
     async getWeather() {
         const data = await Weather.getWeather(this.lat, this.lng);
-
+    
         if (data) {
             const weatherDescription = data.weather?.[0]?.description || 'Weather information not available';
             const weatherCondition = data.weather?.[0]?.main.toLowerCase();
-
+    
             document.querySelector("#weather").innerHTML = weatherDescription;
             document.querySelector("#weather").id = `weather-${weatherCondition}`;
             updateBannerBackground(data.weather?.[0]?.icon || 'Default');
-
-            getQuote()
-                .then(quoteData => {
-                    if (quoteData) {
-                        const quote = document.querySelector("#quote");
-                        quote.innerHTML = quoteData.content;
-                        console.log(quoteData.content);
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+    
+            const quoteType = quoteTypes[weatherCondition] || 'inspire';
+            console.log('Quote Type:', quoteType);
+            console.log('Weather Condition:', weatherCondition);
+    
+            getQuote(weatherCondition)
+            .then(quoteData => {
+                if (quoteData) {
+                    const quote = document.querySelector("#quote");
+                    quote.innerHTML = quoteData.content;
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
         }
-    }
+    }    
 
     errorLocation(err) {
         console.log(err);
