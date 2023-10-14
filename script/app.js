d93b5dd0db35544b0e12d3c71830a9e0b7a95f55
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-14 11:16:14 +0200

    feat: write fetched quote from category

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 import Weather from './weather.js';
 import { updateBannerBackground } from './banner.js';
-import { getQuote, quoteTypes } from './quote.js';
+import { getQuote } from './quote.js';
 
 class App {
     constructor() {
@@ -24,35 +24,39 @@ class App {
 
     async getWeather() {
         const data = await Weather.getWeather(this.lat, this.lng);
-    
+
         if (data) {
             const weatherDescription = data.weather?.[0]?.description || 'Weather information not available';
             const weatherCondition = data.weather?.[0]?.main.toLowerCase();
-    
+
             document.querySelector("#weather").innerHTML = weatherDescription;
             document.querySelector("#weather").id = `weather-${weatherCondition}`;
             updateBannerBackground(data.weather?.[0]?.icon || 'Default');
-    
-            const quoteType = quoteTypes[weatherCondition] || 'inspire';
-            console.log('Quote Type:', quoteType);
-            console.log('Weather Condition:', weatherCondition);
-    
-            getQuote(weatherCondition)
-            .then(quoteData => {
-                if (quoteData) {
-                    const quote = document.querySelector("#quote");
-                    quote.innerHTML = quoteData.content;
-                }
-            })
-            .catch(err => {
-                console.log(err);
-            });
-        }
-    }    
 
-    errorLocation(err) {
-        console.log(err);
+            getQuote(weatherCondition)
+                .then(quoteData => {
+                    if (Array.isArray(quoteData) && quoteData.length > 0) {
+                        const quote = document.querySelector("#quote");
+                        const quoteContent = quoteData[0].content;
+                        quote.innerHTML = quoteContent;
+
+                        const quoteCategory = quoteData[0].tags.join(', ');
+                        console.log('Quote Category:', quoteCategory);
+                        console.log('Quote Content:', quoteContent);
+                    } else {
+                        const quote = document.querySelector("#quote");
+                        quote.innerHTML = 'No quote available for this category.';
+                        console.log('No quote available for this category.');
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }}
+
+        errorLocation(err) {
+            console.log(err);
+        }
     }
-}
 
 let app = new App();
