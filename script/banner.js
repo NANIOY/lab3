c15ee546a8149cb8343384417c9ea8196a1b6431
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-14 11:45:00 +0200

    feat: change jpg to webp

--- script/banner.js
+++ script/banner.js
@@ -1,8 +1,8 @@
 function updateBannerBackground(weatherIcon) {
     // define the background images for each weather condition based on weather icon codes
     const backgroundImages = {
-        '01d': 'url(/images/clear.jpeg)',
-        '01n': 'url(/images/clear.jpeg)',
+        '01d': 'url(/images/clear.webp)',
+        '01n': 'url(/images/clear.webp)',
         '02d': 'url(/images/clouds.webp)',
         '02n': 'url(/images/clouds.webp)',
         '03d': 'url(/images/clouds.webp)',
@@ -13,12 +13,12 @@ function updateBannerBackground(weatherIcon) {
         '09n': 'url(/images/rain.webp)',
         '10d': 'url(/images/rain.webp)',
         '10n': 'url(/images/rain.webp)',
-        '11d': 'url(/images/thunder.jpg)',
-        '11n': 'url(/images/thunder.jpg)',
-        '13d': 'url(/images/snow.jpeg)',
-        '13n': 'url(/images/snow.jpeg)',
-        '50d': 'url(/images/fog.jpg)',
-        '50n': 'url(/images/fog.jpg)',
+        '11d': 'url(/images/thunder.webp)',
+        '11n': 'url(/images/thunder.webp)',
+        '13d': 'url(/images/snow.webp)',
+        '13n': 'url(/images/snow.webp)',
+        '50d': 'url(/images/fog.webp)',
+        '50n': 'url(/images/fog.webp)',
     };
 
     const banner = document.getElementById('weather-banner');
